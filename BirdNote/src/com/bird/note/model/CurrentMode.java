7df6b1d54e5984ebc4eed0caaefd2086b7cbd907
wangxianpeng <xianpeng.wang@nbbsw.com>
2014-12-13 20:46:26 +0800

    fixbugs

--- BirdNote/src/com/bird/note/model/CurrentMode.java
+++
@@ -1,9 +0,0 @@
-package com.bird.note.model;
-
-import com.bird.note.R;
-
-public class CurrentMode {
-	public static final int M_DRAW = R.id.id_edit_title_pen;
-	public static final int M_TEXT = R.id.id_edit_title_text;
-	public static final int M_CLEAN = R.id.id_edit_title_clean;
-}
